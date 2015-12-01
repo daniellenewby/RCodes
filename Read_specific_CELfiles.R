ecc806bbc42aea5b3a2725122958e718cbc39bc2
Danielle Newby <danielleanne.newby@gmail.com>
2015-12-01 14:17:04 +0000

    Update Read_specific_CELfiles.R
    Tidying up code with comments

--- Read_specific_CELfiles.R
+++ Read_specific_CELfiles.R
@@ -12,17 +12,20 @@ library(limma)
 #do not have '/' at the end of this path otherwise the code will not work
 path = '/Users/All Data'
 
-#read in your phenoData with the CEL files you are going to need
-#This contains 1st column - sample names with the title "array", plus a second column (in my example) containing the condition i.e case, control etc the name of this column is not important
-phenoTable <- read.table('/PATH/TO/PHENODATA/phenoData.csv', sep=',', header=TRUE)
+#read in your phenoTable with the CEL files which contains the .CEL samples you are going to need
+#The phenoTable file contains: 1st column - sample names with the title "array", plus a second column
+#containing the condition i.e case, control for each sample - the name of this column is not important for this script
+#you may have more columns in your phenoTable as long as your sample names have the header 'array' the code should work
+phenoTable <- read.table('/PATH/TO/PHENOTABLE/phenoTable.csv', sep=',', header=TRUE)
 
-#create a character vector of the sample CEL file names - your first column must be labelled as 'array'
+#create a character vector of the sample CEL file names - your phenoTable sample name column must be labelled as 'array'
 CELfiles <-as.vector(phenoTable$array)
 
-#most sample names do not have .CEL therefore need to add this in with this step
+#in your phenoTable file your sample names do not have .CEL therefore need to add this on for each sample with this step below
 CELfiles <- paste(CELfiles, ".CEL", sep="")
 
-#normal readAffy however have added in the argument filenames which then only reads in the files in the CELfiles character vector created in previous steps
+#normal readAffy however have added in the argument filenames which then only reads in the files in the CELfiles character 
+#vector created in previous steps
 dataIn <- ReadAffy(filenames=CELfiles, celfile.path=path)
 
 #you can now carry on with the steps with processing in your microarray samples
